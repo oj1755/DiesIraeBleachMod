291f4e79859d66ad969625f4720e0a8458eb553d
lead14 <10saiwatabon@gmail.com>
2023-03-31 19:36:11 +0900

    これって同期できてる？(そっちにTamesiってファイルいった?)

---
+++ src/main/java/net/mcreator/diesiraebleach/procedures/TamesiProcedure.java
@@ -0,0 +1,49 @@
+package net.mcreator.diesiraebleach.procedures;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.DamageSource;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.diesiraebleach.DiesiraebleachMod;
+
+import java.util.Map;
+
+public class TamesiProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				DiesiraebleachMod.LOGGER.warn("Failed to load dependency world for procedure Tamesi!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				DiesiraebleachMod.LOGGER.warn("Failed to load dependency x for procedure Tamesi!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				DiesiraebleachMod.LOGGER.warn("Failed to load dependency y for procedure Tamesi!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				DiesiraebleachMod.LOGGER.warn("Failed to load dependency z for procedure Tamesi!");
+			return;
+		}
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				DiesiraebleachMod.LOGGER.warn("Failed to load dependency entity for procedure Tamesi!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		Entity entity = (Entity) dependencies.get("entity");
+		if (world.isAirBlock(new BlockPos(x, y, z))) {
+			entity.attackEntityFrom(DamageSource.GENERIC, (float) 1);
+		}
+	}
+}
