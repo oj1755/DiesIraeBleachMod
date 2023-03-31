0d17d32884ec5336eaa7f66bf8b759eba9aaac4e
oj1755 <oj175@DESKTOP-NMUM0S1>
2023-03-31 20:11:02 +0900

    tamesiファイルきたよ

--- src/main/java/net/mcreator/diesiraebleach/procedures/BeammahoujinrightclickProcedure.java
+++ src/main/java/net/mcreator/diesiraebleach/procedures/BeammahoujinrightclickProcedure.java
@@ -1,48 +1,10 @@
 package net.mcreator.diesiraebleach.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import java.util.Map;
 
 public class BeammahoujinrightclickProcedure {
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
-		if (dependencies.get("world") == null) {
-			if (!dependencies.containsKey("world"))
-				DiesiraebleachMod.LOGGER.warn("Failed to load dependency world for procedure Beammahoujinrightclick!");
-			return;
-		}
-		if (dependencies.get("x") == null) {
-			if (!dependencies.containsKey("x"))
-				DiesiraebleachMod.LOGGER.warn("Failed to load dependency x for procedure Beammahoujinrightclick!");
-			return;
-		}
-		if (dependencies.get("y") == null) {
-			if (!dependencies.containsKey("y"))
-				DiesiraebleachMod.LOGGER.warn("Failed to load dependency y for procedure Beammahoujinrightclick!");
-			return;
-		}
-		if (dependencies.get("z") == null) {
-			if (!dependencies.containsKey("z"))
-				DiesiraebleachMod.LOGGER.warn("Failed to load dependency z for procedure Beammahoujinrightclick!");
-			return;
-		}
-		if (dependencies.get("entity") == null) {
-			if (!dependencies.containsKey("entity"))
-				DiesiraebleachMod.LOGGER.warn("Failed to load dependency entity for procedure Beammahoujinrightclick!");
-			return;
-		}
-		if (dependencies.get("itemstack") == null) {
-			if (!dependencies.containsKey("itemstack"))
-				DiesiraebleachMod.LOGGER.warn("Failed to load dependency itemstack for procedure Beammahoujinrightclick!");
-			return;
-		}
-
-		IWorld world = (IWorld) dependencies.get("world");
-		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
-		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
-		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
-		Entity entity = (Entity) dependencies.get("entity");
-		ItemStack itemstack = (ItemStack) dependencies.get("itemstack");
-
 		double a = 0;
 		double b = 0;
 		double c = 0;
@@ -58,64 +20,6 @@ public class BeammahoujinrightclickProcedure {
 		double x1 = 0;
 		double y2 = 0;
 		double x2 = 0;
-		if (entity instanceof LivingEntity) {
-			((LivingEntity) entity).swing(Hand.MAIN_HAND, true);
-		}
-		if (itemstack.getOrCreateTag().getDouble("kill") <= 0) {
-			itemstack.getOrCreateTag().putDouble("kill", 30);
-			itemstack.getOrCreateTag().putDouble("killr", 4);
-			itemstack.getOrCreateTag().putDouble("killa", (entity.rotationYaw));
-			itemstack.getOrCreateTag().putDouble("killb", (entity.rotationPitch));
-			itemstack.getOrCreateTag().putDouble("killx", x);
-			itemstack.getOrCreateTag().putDouble("killy", y);
-			itemstack.getOrCreateTag().putDouble("killz", z);
-			r = 1;
-			s = 2;
-			a = (entity.rotationYaw);
-			b = (entity.rotationPitch);
-			c = 0;
-			for (int index0 = 0; index0 < (int) (60); index0++) {
-				world.addParticle(ParticleTypes.END_ROD,
-						(x - (r * Math.cos(Math.toRadians(b)) * Math.sin(Math.toRadians(a))
-								+ s * Math.cos(Math.toRadians(c)) * Math.cos(Math.toRadians(b - 90)) * Math.sin(Math.toRadians(a))
-								+ s * Math.sin(Math.toRadians(c)) * Math.sin(Math.toRadians(a - 90)))),
-						((y + 1) - (r * Math.sin(Math.toRadians(b)) + s * Math.cos(Math.toRadians(c)) * Math.sin(Math.toRadians(b - 90)))),
-						(z + r * Math.cos(Math.toRadians(b)) * Math.cos(Math.toRadians(a))
-								+ s * Math.cos(Math.toRadians(c)) * Math.cos(Math.toRadians(b - 90)) * Math.cos(Math.toRadians(a))
-								+ s * Math.sin(Math.toRadians(c)) * Math.cos(Math.toRadians(a - 90))),
-						0, 0, 0);
-				c = (c + 6);
-			}
-			c = 0;
-			i = 50;
-			for (int index1 = 0; index1 < (int) (5); index1++) {
-				c = c;
-				x1 = (x - (r * Math.cos(Math.toRadians(b)) * Math.sin(Math.toRadians(a))
-						+ s * Math.cos(Math.toRadians(c)) * Math.cos(Math.toRadians(b - 90)) * Math.sin(Math.toRadians(a))
-						+ s * Math.sin(Math.toRadians(c)) * Math.sin(Math.toRadians(a - 90))));
-				x2 = (x - (r * Math.cos(Math.toRadians(b)) * Math.sin(Math.toRadians(a))
-						+ s * Math.cos(Math.toRadians(c + 144)) * Math.cos(Math.toRadians(b - 90)) * Math.sin(Math.toRadians(a))
-						+ s * Math.sin(Math.toRadians(c + 144)) * Math.sin(Math.toRadians(a - 90))));
-				y1 = ((y + 1) - (r * Math.sin(Math.toRadians(b)) + s * Math.cos(Math.toRadians(c)) * Math.sin(Math.toRadians(b - 90))));
-				y2 = ((y + 1) - (r * Math.sin(Math.toRadians(b)) + s * Math.cos(Math.toRadians(c + 144)) * Math.sin(Math.toRadians(b - 90))));
-				z1 = (z + r * Math.cos(Math.toRadians(b)) * Math.cos(Math.toRadians(a))
-						+ s * Math.cos(Math.toRadians(c)) * Math.cos(Math.toRadians(b - 90)) * Math.cos(Math.toRadians(a))
-						+ s * Math.sin(Math.toRadians(c)) * Math.cos(Math.toRadians(a - 90)));
-				z2 = (z + r * Math.cos(Math.toRadians(b)) * Math.cos(Math.toRadians(a))
-						+ s * Math.cos(Math.toRadians(c + 144)) * Math.cos(Math.toRadians(b - 90)) * Math.cos(Math.toRadians(a))
-						+ s * Math.sin(Math.toRadians(c + 144)) * Math.cos(Math.toRadians(a - 90)));
-				dx = ((x2 - x1) / i);
-				dy = ((y2 - y1) / i);
-				dz = ((z2 - z1) / i);
-				for (int index2 = 0; index2 < (int) (i); index2++) {
-					world.addParticle(ParticleTypes.END_ROD, x1, y1, z1, 0, 0, 0);
-					x1 = (x1 + dx);
-					y1 = (y1 + dy);
-					z1 = (z1 + dz);
-				}
-				c = (c + 72);
-			}
-		}
-	}
 
+	}
 }
--- src/main/java/net/mcreator/diesiraebleach/procedures/BeammahoujinuptickProcedure.java
+++ src/main/java/net/mcreator/diesiraebleach/procedures/BeammahoujinuptickProcedure.java
@@ -1,15 +1,103 @@
 package net.mcreator.diesiraebleach.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.DamageSource;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.diesiraebleach.DiesiraebleachMod;
+
+import java.util.stream.Collectors;
+import java.util.function.Function;
+import java.util.Map;
+import java.util.List;
+import java.util.Comparator;
 
 public class BeammahoujinuptickProcedure {
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
-
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				DiesiraebleachMod.LOGGER.warn("Failed to load dependency world for procedure Beammahoujinuptick!");
+			return;
+		}
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				DiesiraebleachMod.LOGGER.warn("Failed to load dependency entity for procedure Beammahoujinuptick!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		Entity entity = (Entity) dependencies.get("entity");
 		double a = 0;
 		double r = 0;
 		double b = 0;
-
+		if (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getItem() == Blocks.AIR.asItem()) {
+			if (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+					.getDouble("kill") > 0) {
+				if (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+						.getDouble("kill") > 25) {
+					((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+							.putDouble("kill", (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+									.getOrCreateTag().getDouble("kill") - 1));
+				} else {
+					r = (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+							.getDouble("killr"));
+					a = (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+							.getDouble("killa"));
+					b = (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+							.getDouble("killb"));
+					{
+						List<Entity> _entfound = world.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(
+								(((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+										.getDouble("killx") - r * Math.cos(Math.toRadians(b)) * Math.sin(Math.toRadians(a))) - (4 / 2d),
+								((((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+										.getOrCreateTag().getDouble("killy") + 1) - r * Math.sin(Math.toRadians(b))) - (4 / 2d),
+								(((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+										.getDouble("killz") + r * Math.cos(Math.toRadians(b)) * Math.cos(Math.toRadians(a))) - (4 / 2d),
+								(((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+										.getDouble("killx") - r * Math.cos(Math.toRadians(b)) * Math.sin(Math.toRadians(a))) + (4 / 2d),
+								((((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+										.getOrCreateTag().getDouble("killy") + 1) - r * Math.sin(Math.toRadians(b))) + (4 / 2d),
+								(((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+										.getDouble("killz") + r * Math.cos(Math.toRadians(b)) * Math.cos(Math.toRadians(a))) + (4 / 2d)),
+								null).stream().sorted(new Object() {
+									Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+										return Comparator.comparing((Function<Entity, Double>) (_entcnd -> _entcnd.getDistanceSq(_x, _y, _z)));
+									}
+								}.compareDistOf(
+										(((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+												.getOrCreateTag().getDouble("killx") - r * Math.cos(Math.toRadians(b)) * Math.sin(Math.toRadians(a))),
+										((((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+												.getOrCreateTag().getDouble("killy") + 1) - r * Math.sin(Math.toRadians(b))),
+										(((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+												.getOrCreateTag().getDouble("killz")
+												+ r * Math.cos(Math.toRadians(b)) * Math.cos(Math.toRadians(a)))))
+								.collect(Collectors.toList());
+						for (Entity entityiterator : _entfound) {
+							entityiterator.attackEntityFrom(DamageSource.GENERIC, (float) 10);
+						}
+					}
+					for (int index0 = 0; index0 < (int) (50); index0++) {
+						world.addParticle(KillparticleParticle.particle,
+								(((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+										.getDouble("killx") - r * Math.cos(Math.toRadians(b)) * Math.sin(Math.toRadians(a))),
+								((((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+										.getOrCreateTag().getDouble("killy") + 1) - r * Math.sin(Math.toRadians(b))),
+								(((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+										.getDouble("killz") + r * Math.cos(Math.toRadians(b)) * Math.cos(Math.toRadians(a))),
+								0, 0, 0);
+						r = (r + 0.02);
+					}
+					((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+							.putDouble("killr", r);
+					((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getOrCreateTag()
+							.putDouble("kill", (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+									.getOrCreateTag().getDouble("kill") - 1));
+				}
+			}
+		}
 	}
-
 }
