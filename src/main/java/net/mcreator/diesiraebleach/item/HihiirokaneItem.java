cf5d14e554a67b29917acbd8bc4f2d8d4408e473
oj1755 <oj175@DESKTOP-NMUM0S1>
2023-04-03 20:30:14 +0900

    hihiirokane

---
+++ src/main/java/net/mcreator/diesiraebleach/item/HihiirokaneItem.java
@@ -0,0 +1,60 @@
+
+package net.mcreator.diesiraebleach.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.diesiraebleach.DiesiraebleachModElements;
+
+@DiesiraebleachModElements.ModElement.Tag
+public class HihiirokaneItem extends DiesiraebleachModElements.ModElement {
+	@ObjectHolder("diesiraebleach:hihiirokane")
+	public static final Item block = null;
+
+	public HihiirokaneItem(DiesiraebleachModElements instance) {
+		super(instance, 19);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 6f;
+			}
+
+			public float getAttackDamage() {
+				return 13f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, -2f, new Item.Properties().group(ItemGroup.COMBAT).isImmuneToFire()) {
+			@Override
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasEffect(ItemStack itemstack) {
+				return true;
+			}
+		}.setRegistryName("hihiirokane"));
+	}
+}
